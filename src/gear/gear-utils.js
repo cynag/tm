ff9e0003c74c54d21725224ffe288764cd4c0815
cynag <gustavossrochass@gmail.com>
2025-06-16 04:02:29 -0300

    1.6b
    apenas alguns ajustes

--- src/gear/gear-utils.js
+++ src/gear/gear-utils.js
@@ -5,7 +5,9 @@ export class GearUtils {
     const rule = GearConstants.GEAR_SLOT_RULES[slotId];
     if (!rule) return false;
     if (item.type !== "gear") return false;
-    if (rule.type && item.system.gear_type !== rule.type) return false;
+if (rule.type && item.system.gear_type !== rule.type) return false;
+if (rule.subtype && item.system.subtype !== rule.subtype) return false;
+
     return true;
   }
 }
