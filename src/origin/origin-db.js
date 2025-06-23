758ca4029c47f1fee176a1f074cd2a8c01530054
cynag <gustavossrochass@gmail.com>
2025-06-23 01:33:25 -0300

    Adicionadaorigens e tags à ficha de personagem e estilos

--- src/origin/origin-db.js
+++ src/origin/origin-db.js
@@ -1,22 +1,100 @@
 export const OriginDB = [
   {
-    id: "nobre",
-    name: "Nobre",
-    img: "systems/tm/assets/origin/nobre.webp",
-    description: "Você cresceu entre os privilégios da aristocracia ou da elite dominante.",
-    origin_effect_script: `async function(actor) {
-      actor.system.player_letality += 100;
-      actor.system.player_virtue += 100;
-    }`
+    id: "acolito",
+    name: "Acólito",
+    img: "systems/tm/assets/origin/acolito.webp",
+    description: "Você viveu grande parte da vida como um seguidor à serviço da fé. Poderá ter vivido em um templo remoto junto a outros da sua fé, ou até mesmo se-guindo seu propósito em isolamento e provação. Durante este período, você fez votos, rituais e sacri-fícios próprios conforme sua Verdade. ",
+    origin_effect_script: `async function(actor) {}`
   },
   {
-    id: "batedor",
-    name: "Batedor",
-    img: "systems/tm/assets/origin/batedor.webp",
-    description: "Você foi treinado para se mover rápido, detectar perigos e sobreviver em território hostil.",
-    origin_effect_script: `async function(actor) {
-      actor.system.player_dexterity += 100;
-      actor.system.player_impulse += 100;
-    }`
+    id: "arcanista",
+    name: "Arcanista",
+    img: "systems/tm/assets/origin/arcanista.webp",
+    description: "Você desde muito novo demonstrou aptidão ao domínio da Arcana. Seu primórdio foi notável atra-vés de surtos espontâneos de magia ou um interes-se sobrenatural no assunto. Você pode ter vivido em uma academia arcana, ou ter tido um tutor des-de cedo, que notou seu potencial.",
+    origin_effect_script: `async function(actor) {}`
+  },
+  {
+    id: "artesao",
+    name: "Artesão",
+    img: "systems/tm/assets/origin/artesao.webp",
+    description: "Você dedicou seu passado à aprender a arte de um ofício, calejando seus dedos e praticando até o cair da noite para dominar a habilidade de criar algo bem feito. Você pode ter vindo de uma família de artesões, seguindo o legado familiar. Ou pode ter sido aprendiz de alguém renomado, como um fer-reiro, um alfaiate, um cozinheiro ou um alquimista.",
+    origin_effect_script: `async function(actor) {}`
+  },
+  {
+    id: "artista",
+    name: "Artista",
+    img: "systems/tm/assets/origin/xxx.webp",
+    description: "Você dedicou-se de corpo e alma a aprender o domínio da Arte, descobrindo sua beleza e exube-rância em todas as áreas. Pode ter sido aluno em um conservatório de bardos, um ator que estudou com os melhores mestres de corte, ou um aprendiz de um renomado ateliê de pintura. Seja qual forma a arte se manifestou, deixou uma marca eterna em sua vida.  ",
+    origin_effect_script: `async function(actor) {}`
+  },
+  {
+    id: "beligerante",
+    name: "Beligerante",
+    img: "systems/tm/assets/origin/beligerante.webp",
+    description: "Você esteve ao longo de sua vida sempre rodeado de guerreiros, histórias e guerras, muitos testemunha-dos, e alguns até praticante. Você pode ser filho de um oficial, ou um guerreiro empenhado, ou até mesmo, apenas parte da turba que acompanhava os exércitos em suas guerras. Você pode ter sido criado em acampamentos, castelos ou tribos selvagens.",
+    origin_effect_script: `async function(actor) {}`
+  },
+  {
+    id: "caixeiro-viajante",
+    name: "Caixeiro Viajante",
+    img: "systems/tm/assets/origin/caixeiro.webp",
+    description: "Você lidou com o mercantilismo desde muito cedo. Fazendo parte de um negócio de família, ou traba-lhando com um vendedor contratado. Você pode ter tentando a sorte nos grandes mercados das ci-dades, ou até mesmo participado de caravanas mer-cantis que viajaram pelo mundo. Você aprendeu a lei do comércio, e sabe se virar quando o assunto é vender ou pechinchar. ",
+    origin_effect_script: `async function(actor) {}`
+  },
+   {
+    id: "eremita",
+    name: "Eremita",
+    img: "systems/tm/assets/origin/eremita.webp",
+    description: "Você passou boa parte da vida afastado de qualquer civilização. Você pode ter sido criado em uma co-munidade isolada, pode ter se perdido na floresta e ter sido adotado por outros eremitas, ou até por seres mágicos. Ou, pode simplesmente ter escolhido ficar longe de tudo e todos. Em sua solidão, apren-deu a valorizar coisas que muitos não se importam. Por outro lado, conviver em conjunto agora é um desafio. ",
+    origin_effect_script: `async function(actor) {}`
+  },
+   {
+    id: "estudioso",
+    name: "Estudioso",
+    img: "systems/tm/assets/origin/estudioso.webp",
+    description: "Você passou parte da vida imerso em pilhas gigantescas de livros. Buscando conhecimento so-bre o presente, passado, e talvez, futuro. Você pode ter sido criado em um orfanato de monges copistas, ou ser alguém abastado para poder financeiramente estar imerso aos livros. Agora, nesta nova fase da vida, muito do que foi estudo lhe será útil.",
+    origin_effect_script: `async function(actor) {}`
+  },
+   {
+    id: "gatuno",
+    name: "Gatuno",
+    img: "systems/tm/assets/origin/gatuno.webp",
+    description: "Você cresceu em uma grande cidade e enfrentou pobreza e falta de oportunidades, se tornando uma trombadinha. Você pode ter sido recrutado por criminosos, sendo introduzido desde jovem ao mundo do crime, ou simplesmente ter começado a roubar por instinto. Você aprendeu habilidades sórdidas, como furtar bolsas, manipular pessoas e andar sorrateiramente em locais escuros.  ",
+    origin_effect_script: `async function(actor) {}`
+  },
+   {
+    id: "marujo",
+    name: "Marujo",
+    img: "systems/tm/assets/origin/marujo.webp",
+    description: "Você cresceu com o ritmo das marés, tendo uma relação estreita com o Oceano. Você ode ter de-sempenhado funções como um aprendiz de mari-nheiro, embarcado em navios de pesca, ou pode ter sido recrutado por trupes de piratas contrabandis-tas. Sua existência foi moldada pelo sacolejar das embarcações.",
+    origin_effect_script: `async function(actor) {}`
+  },
+   {
+    id: "nobre",
+    name: "Nobre",
+    img: "systems/tm/assets/origin/xxx.webp",
+    description: "Você nasceu em um ninho de ouro. Criado com cuidado pela elite aristocrata. Você teve tudo do bom e do melhor em sua vida; recebeu educação sofisticada, cuidados especializados e tudo mais que uma vida abastada por permitir. Considere-se sor-tudo, você já teve luxo que a maior parte do mundo nunca vai poder sonhar em ter.   ",
+    origin_effect_script: `async function(actor) {}`
+  },
+   {
+    id: "nomade",
+    name: "Nômade",
+    img: "systems/tm/assets/origin/nomade.webp",
+    description: "Você pertenceu a uma comunidade simples que vivia no estilo nômade. Nunca se prendeu a um local e aprendeu que as vezes é melhor se desape-gar de algumas coisas. Você mudou sua vida con-forme a escassez, mudando de local em todos os anos. Você pode ter sido parte de um grupo de pe-regrinos itinerantes, comerciantes, ou um selvagem que nunca praticou agricultura. ",
+    origin_effect_script: `async function(actor) {}`
+  },
+   {
+    id: "plebeu",
+    name: "Plebeu",
+    img: "systems/tm/assets/origin/xxx.webp",
+    description: "Você nasceu na grande e vasta vida ordinária, em um pequeno vilarejo, província ou feudo. Você pas-sou muitos anos de sua vida em fazenda, trabalhan-do em plantações, cuidando de animais e moendo grãos. Você também pode ter trabalhado para co-merciantes locais, em carvoarias e serrarias. Você bebeu cerveja quente nas tavernas após o término do árduo expediente, e apesar do cansaço inerente, aprendeu muitas coisas que se mostrarão valiosas em sua vida de aventureiro. ",
+    origin_effect_script: `async function(actor) {}`
+  },
+   {
+    id: "trapaceiro",
+    name: "Trapaceiro",
+    img: "systems/tm/assets/origin/trapaceiro.webp",
+    description: "Você viveu a vida no meio das artimanhas. Se tornando um hábil charlatão que ludibriava pes-soas, enganando e explorando inúmeras pessoas em seu benefício. Você pode ter sido influenciado por seus pais, que o ensinaram- ou, talvez, ter aprendi-do tudo isso por lazer. Você se tornou um grande manipulador, aplicando jogos de azar em bêbados e entrando disfarçado em grandes festejos particula-res",
+    origin_effect_script: `async function(actor) {}`
   }
 ];
